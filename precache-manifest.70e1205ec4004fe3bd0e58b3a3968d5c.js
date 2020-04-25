252ad0dad003cfb1be337d3ad019248f044ae944
ivan-0810 <48289492+ivan-0810@users.noreply.github.com>
2020-04-25 12:45:18 +0200

    Updates

---
+++ precache-manifest.70e1205ec4004fe3bd0e58b3a3968d5c.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a32127b358e563de1904723d50b1eab",
+    "url": "/brainster-space/index.html"
+  },
+  {
+    "revision": "4251514433d1a9f3c054",
+    "url": "/brainster-space/static/css/main.58d0c783.chunk.css"
+  },
+  {
+    "revision": "a757a4dce2fff9403991",
+    "url": "/brainster-space/static/js/2.7304df12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brainster-space/static/js/2.7304df12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4251514433d1a9f3c054",
+    "url": "/brainster-space/static/js/main.c6c46aba.chunk.js"
+  },
+  {
+    "revision": "e68d14c4f8a3f131b1a7",
+    "url": "/brainster-space/static/js/runtime-main.3880a7ab.js"
+  },
+  {
+    "revision": "09b258d80ba26b14b6edd67b6c262984",
+    "url": "/brainster-space/static/media/1.09b258d8.jpg"
+  },
+  {
+    "revision": "5411d42f905be25f8061162dcecf7a3c",
+    "url": "/brainster-space/static/media/2.5411d42f.jpg"
+  },
+  {
+    "revision": "e3abc4d2ed4c83ae5025d0aff4c2f064",
+    "url": "/brainster-space/static/media/2.e3abc4d2.jpg"
+  },
+  {
+    "revision": "f69a3cbd6d5a16b41100c095f19d4a47",
+    "url": "/brainster-space/static/media/3.f69a3cbd.jpg"
+  },
+  {
+    "revision": "dd8f0427ee91b7d416878acf8d7ccaee",
+    "url": "/brainster-space/static/media/4.dd8f0427.jpg"
+  },
+  {
+    "revision": "6b99e524c752ddb1cb53e518e13d00aa",
+    "url": "/brainster-space/static/media/5.6b99e524.jpg"
+  },
+  {
+    "revision": "5bb2b357abfa09af0349cc418efface3",
+    "url": "/brainster-space/static/media/6.5bb2b357.jpg"
+  },
+  {
+    "revision": "c8b1076de3d46dcd60007fd89f366162",
+    "url": "/brainster-space/static/media/7.c8b1076d.jpg"
+  },
+  {
+    "revision": "0d331d221716b84c5a9a2765a098e43c",
+    "url": "/brainster-space/static/media/8.0d331d22.jpg"
+  },
+  {
+    "revision": "5d8755de6e01bdcfc83427f9a4c5278d",
+    "url": "/brainster-space/static/media/9.5d8755de.jpg"
+  },
+  {
+    "revision": "5f0aea753e907874b11f71cc5017ef7f",
+    "url": "/brainster-space/static/media/C1 1.5f0aea75.jpg"
+  },
+  {
+    "revision": "70680b3ede7d05def8be070bb4e50368",
+    "url": "/brainster-space/static/media/C1 4.70680b3e.jpg"
+  },
+  {
+    "revision": "87056f6fcb839248ea832893b5bd0f97",
+    "url": "/brainster-space/static/media/C2 1.87056f6f.jpg"
+  },
+  {
+    "revision": "f126f6354de092e5fb19252576291a52",
+    "url": "/brainster-space/static/media/C3 1.f126f635.jpg"
+  },
+  {
+    "revision": "2ed9fb5e486dcfb1cc6df01ea8afde93",
+    "url": "/brainster-space/static/media/C3 2.2ed9fb5e.jpg"
+  },
+  {
+    "revision": "345908b7701b6e02de5034ec669bfd64",
+    "url": "/brainster-space/static/media/C3 EXIBITION_1.345908b7.jpg"
+  },
+  {
+    "revision": "466926fe882d15de7069f8636c376fff",
+    "url": "/brainster-space/static/media/HOL KON SEDENJE.466926fe.jpg"
+  },
+  {
+    "revision": "b207a80e561262484fb745fe1b1ef070",
+    "url": "/brainster-space/static/media/Hristijan-Nosecka-1024x536.b207a80e.jpg"
+  },
+  {
+    "revision": "6f12f9552dd0f8ce9c7effba4a267d21",
+    "url": "/brainster-space/static/media/IMG_7361.6f12f955.jpg"
+  },
+  {
+    "revision": "9ff840cb1ff16fa581ba68e5f66f412e",
+    "url": "/brainster-space/static/media/IMG_7362.9ff840cb.jpg"
+  },
+  {
+    "revision": "4fb3fa9b5edea51fe8e971e9d767e945",
+    "url": "/brainster-space/static/media/IMG_7385.4fb3fa9b.jpg"
+  },
+  {
+    "revision": "b61a01e02270bbc444439380df7b8497",
+    "url": "/brainster-space/static/media/IMG_7481.b61a01e0.jpg"
+  },
+  {
+    "revision": "051cc5c5e6923ebd52aa7cb148aa92f0",
+    "url": "/brainster-space/static/media/IMG_7777.051cc5c5.jpg"
+  },
+  {
+    "revision": "5325200aaa34da19fd50f2a9e4082139",
+    "url": "/brainster-space/static/media/KITCHEN_03.5325200a.jpg"
+  },
+  {
+    "revision": "090ce3d30081270fd168995d6d18dc1b",
+    "url": "/brainster-space/static/media/brainster_space_logo.090ce3d3.svg"
+  },
+  {
+    "revision": "85d74911ceb353de05e8e10f9768e05e",
+    "url": "/brainster-space/static/media/coworking.85d74911.jpg"
+  },
+  {
+    "revision": "83ab592af245e50a96d436a3cb47e595",
+    "url": "/brainster-space/static/media/default.83ab592a.png"
+  },
+  {
+    "revision": "cc7b914b6a75a4fa36807dfb25576a91",
+    "url": "/brainster-space/static/media/devojka.cc7b914b.jpg"
+  },
+  {
+    "revision": "af6db5af27a53bbbfead8bc81f68b919",
+    "url": "/brainster-space/static/media/edukacija.af6db5af.jpg"
+  },
+  {
+    "revision": "ada24cb5090ddf3073b2ef89408fb997",
+    "url": "/brainster-space/static/media/edukacija1.ada24cb5.jpg"
+  },
+  {
+    "revision": "7a8f072bfa159f15d3784aefb20f2324",
+    "url": "/brainster-space/static/media/edukacija2.7a8f072b.jpg"
+  },
+  {
+    "revision": "fcb54dfcfb59955b76e6dd4455ebf43a",
+    "url": "/brainster-space/static/media/instruktori.fcb54dfc.jpg"
+  },
+  {
+    "revision": "b33d5f90e0cd3be249b6f44e35d1ac71",
+    "url": "/brainster-space/static/media/nastani.b33d5f90.jpg"
+  },
+  {
+    "revision": "1dc7caf0d8dc89a8c1290f986c342f2c",
+    "url": "/brainster-space/static/media/prostor za nastani.1dc7caf0.jpg"
+  }
+]);
